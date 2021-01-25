e71690d90c885fe9503443d610d701ee8abf1ec9
Nidhi Desai <nidhidesai5499@gmail.com>
2021-01-25 12:22:06 +0530

    What is data science?

---
+++ ds.R
@@ -0,0 +1,3 @@
+print("Data science is an inter-disciplinary field that uses scientific methods, processes, algorithms and systems to extract knowledge and insights from many structural and unstructured data.Data science is related to data mining, machine learning and big data.")
+
+print("Data science is a concept to unify statistics, data analysis and their related methods in order to understand and analyze actual phenomena with data.It uses techniques and theories drawn from many fields within the context of mathematics, statistics, computer science, domain knowledge and information science.")
